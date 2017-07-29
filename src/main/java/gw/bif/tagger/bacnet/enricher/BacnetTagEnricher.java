d95bc4f8c6db11b0e19877d55b0146a169bf78b1
Subhadip Pramanik <subhadip@Subhadips-MacBook-Pro.local>
2017-07-29 14:41:56 +0530

    chore: remove unused code

--- src/main/java/gw/bif/tagger/bacnet/enricher/BacnetTagEnricher.java
+++ src/main/java/gw/bif/tagger/bacnet/enricher/BacnetTagEnricher.java
@@ -1,15 +1,10 @@
 package gw.bif.tagger.bacnet.enricher;
 
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
-
-import com.google.common.collect.ImmutableList;
-
-import gw.bif.tagger.core.message.Tag;
 
 @Component
 public class BacnetTagEnricher {
@@ -22,14 +17,5 @@ public class BacnetTagEnricher {
 		}
 		logger.info(message);
 		return message;
-	}
-	
-	public List<Tag> enrich(Tag message) {
-		
-		if("bacnet:present-value".equals(message.getKey())) {
-			return ImmutableList.of(message, new Tag("bacnet:currVal", message.getValue()));
-		}
-		logger.info(message);
-		return ImmutableList.of(message);
 	}
 }
