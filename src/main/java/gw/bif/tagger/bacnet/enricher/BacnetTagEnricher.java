b689e4642a9d2ab402664a21972cc4e57b71ade1
Subhadip <subhadip.pramanik@siemens.com>
2017-07-31 16:28:21 +0530

    feat: read mutiple tags against a single property

--- src/main/java/gw/bif/tagger/bacnet/enricher/BacnetTagEnricher.java
+++ src/main/java/gw/bif/tagger/bacnet/enricher/BacnetTagEnricher.java
@@ -1,8 +1,8 @@
 package gw.bif.tagger.bacnet.enricher;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,10 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.ImmutableList;
+
 @Component
 public class BacnetTagEnricher {
 
+	private static final String NAMESPACE_SEPARATOR = ":";
+
 	Log logger = LogFactory.getLog(this.getClass());
+	
 
 	@Autowired
 	Environment env;
@@ -22,9 +27,9 @@ public class BacnetTagEnricher {
 		Map<String, String> richMessage = new HashMap<>();
 		message.keySet().forEach(key -> {
 			richMessage.put(key, message.get(key));
-			Optional<String> extraTag = findTagFor(key);
-			if (extraTag.isPresent()) {
-				richMessage.put(extraTag.get(), message.get(key));
+			List<String> extraTags = findTagFor(key);
+			if (!extraTags.isEmpty()) {
+				extraTags.forEach(tag -> richMessage.put(tag, message.get(key)));				
 			}
 
 		});
@@ -32,14 +37,16 @@ public class BacnetTagEnricher {
 		return richMessage;
 	}
 
-	private Optional<String> findTagFor(String key) {
-		String namespace = key.split(":")[0];
-		String propertyName = key.split(":")[1];
-		String tag = new TagFinder().forProperty(propertyName).in(env).find();
-		if(tag.equals(TagFinder.NOT_FOUND)) {
-			return Optional.empty();
+	private List<String> findTagFor(String key) {
+		String[] splitBySeparator = key.split(NAMESPACE_SEPARATOR);
+		if (splitBySeparator.length > 1) {
+			String propertyName = splitBySeparator[1];
+			List<String> tags = new TagFinder().forProperty(propertyName).in(env).find();
+			if (!tags.get(0).equals(TagFinder.NOT_FOUND)) {
+				return tags;
+			}			
 		}
-		return Optional.of(namespace + ":" + tag);
+		return ImmutableList.of();
 	}
 
 	private class TagFinder {
@@ -65,8 +72,8 @@ public class BacnetTagEnricher {
 			return this;
 		}
 
-		private String find() {
-			return environment.getProperty(originalTag, defaultValue);
+		private List<String> find() {
+			return ImmutableList.copyOf(environment.getProperty(originalTag, defaultValue).split(","));
 		}
 	}
 }
