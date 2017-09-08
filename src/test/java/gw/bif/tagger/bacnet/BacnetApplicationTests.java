0ecab59f481b8384f47220f739374aea89c5c633
Subhadip Pramanik <pramaniksubhadip@gmail.com>
2017-09-09 01:03:12 +0530

    test:dummy test

--- src/test/java/gw/bif/tagger/bacnet/BacnetApplicationTests.java
+++ src/test/java/gw/bif/tagger/bacnet/BacnetApplicationTests.java
@@ -1,16 +1,16 @@
 package gw.bif.tagger.bacnet;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(BlockJUnit4ClassRunner.class)
 public class BacnetApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void dummyTest() {
+		Assert.assertTrue(Boolean.TRUE);
 	}
 
 }
